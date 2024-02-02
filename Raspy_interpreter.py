49bde4607cc1814ef106a3b105d8e278b9837d7c
Milenium7 <86931512+Milenium7@users.noreply.github.com>
2024-02-02 23:28:28 +0200

    Create Raspy_interpreter.py
    The core of raspy

---
+++ Raspy_interpreter.py
@@ -0,0 +1,147 @@
+import RPi.GPIO as GPIO
+import time
+import random
+import math
+
+variables = {}
+gpio_pins = {}
+
+def execute_gpio(keyword, *args):
+    if keyword == 'setup':
+        pin = int(args[0])
+        mode = args[1]
+        if mode == 'input':
+            GPIO.setup(pin, GPIO.IN)
+        elif mode == 'output':
+            GPIO.setup(pin, GPIO.OUT)
+
+    elif keyword == 'high':
+        pin = int(args[0])
+        GPIO.output(pin, GPIO.HIGH)
+
+    elif keyword == 'low':
+        pin = int(args[0])
+        GPIO.output(pin, GPIO.LOW)
+
+    elif keyword == 'pulse':
+        pin = int(args[0])
+        duration = float(args[1])
+
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(duration)
+        GPIO.output(pin, GPIO.LOW)
+
+    elif keyword == 'read':
+        pin = int(args[0])
+        return GPIO.input(pin)
+
+def execute_string(keyword, *args):
+    pass
+
+def execute_console(keyword, *args):
+    pass
+
+def execute_math(keyword, *args):
+    pass
+
+def interpret(code):
+    global variables, gpio_pins
+
+    lines = code.split('\n')
+
+    for line in lines:
+        tokens = line.split()
+
+        if not tokens:
+            continue
+
+        keyword = tokens[0]
+
+        if keyword == 'print':
+            print(' '.join(tokens[1:]))
+
+        elif keyword == 'functions':
+            # Define functions here if needed
+            pass
+
+        elif keyword == 'if':
+            # Implement if statement
+            pass
+
+        elif keyword == 'var':
+            var_name = tokens[1]
+            var_value = ' '.join(tokens[3:])
+            variables[var_name] = var_value
+
+        elif keyword == 'input':
+            var_name = tokens[1]
+            user_input = input(f'Enter value for {var_name}: ')
+            variables[var_name] = user_input
+
+        elif keyword == 'class':
+            # Implement class definition
+            pass
+
+        elif keyword == 'rpi':
+            gpio_pins = {int(pin): False for pin in tokens[1:]}
+            for pin in gpio_pins:
+                execute_gpio('setup', pin, 'output')
+
+        elif keyword == 'gpio':
+            result = execute_gpio(tokens[1], *tokens[2:])
+            if result is not None:
+                print(result)
+
+        elif keyword == 'string':
+            result = execute_string(tokens[1], *tokens[2:])
+            if result is not None:
+                print(result)
+
+        elif keyword == 'console':
+            result = execute_console(tokens[1], *tokens[2:])
+            if result is not None:
+                print(result)
+
+        elif keyword == 'math':
+            result = execute_math(tokens[1], *tokens[2:])
+            if result is not None:
+                print(result)
+
+        elif keyword == 'random':
+            # Implement random functions
+            pass
+
+        elif keyword == 'while':
+            # Implement while loop
+            pass
+
+        elif keyword == 'type':
+            # Implement type function
+            pass
+
+        elif keyword == 'range':
+            # Implement range function
+            pass
+
+        elif keyword == 'for':
+            # Implement for loop
+            pass
+
+# Example code in the custom language
+code = """
+var x = 10
+var y = 20
+print x
+print y
+rpi 18
+gpio pulse 18 2
+"""
+
+# Initialize GPIO
+GPIO.setmode(GPIO.BCM)
+
+# Interpret the code
+interpret(code)
+
+# Cleanup GPIO
+GPIO.cleanup()
